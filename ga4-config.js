e57fe0b7a50c0f21252c4602e7e261da98f5f433
jadeldiaz01-png <jadeldiaz01@gmail.com>
2026-09-20 11:47:38 -0400

    analytics: bind verified GA4 id while keeping loader disabled

--- ga4-config.js
+++ ga4-config.js
@@ -1,7 +1,7 @@
 window.JADEL_GA4_CONFIG = Object.freeze({
-  state: "MEASUREMENT_ID_REQUIRED",
+  state: "MEASUREMENT_ID_CONFIRMED_PENDING_VALIDATION",
   enabled: false,
-  measurementId: "",
+  measurementId: "G-K60SQ2ZHL9",
   consentRequired: true,
   debugMode: false
 });
